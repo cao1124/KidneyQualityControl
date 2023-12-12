b19ad52d3268ef9c945f2c5d4bde1c18b8ff034e
cao <cao_1124@163.com>
2023-12-12 17:14:39 +0800

    20231204-mass-segment fix

--- segment_util.py
+++ segment_util.py
@@ -264,12 +264,14 @@ class RenalDataset(BaseDataset):
     ):
         self.images, self.masks = [], []
         for i in range(len(images_dir)):
-            for img_name in os.listdir(os.path.join(images_dir[i])):
-                self.images.append(os.path.join(images_dir[i], img_name))
-                if '.JPG' in img_name:
-                    self.masks.append(os.path.join(masks_dir[i], img_name.replace('.JPG', '.jpg')))
-                else:
-                    self.masks.append(os.path.join(masks_dir[i], img_name))
+            for cla in os.listdir(images_dir[i]):
+                for p in os.listdir(os.path.join(images_dir[i], cla)):
+                    for img_name in os.listdir(os.path.join(images_dir[i], cla, p)):
+                        self.images.append(os.path.join(images_dir[i], cla, p, img_name))
+                        if '.JPG' in img_name:
+                            self.masks.append(os.path.join(masks_dir[i], img_name.replace('.JPG', '.jpg')))
+                        else:
+                            self.masks.append(os.path.join(masks_dir[i], img_name))
         self.augmentation = augmentation
         self.preprocessing = preprocessing
         self.multi_scale = multi_scale
