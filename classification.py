d11b4028ec9bfdd10fe88eab4576f53dbccdbefd
cao <cao_1124@163.com>
2024-03-14 11:49:39 +0800

    20240314-mass-crop-classify-densenet161

--- classification.py
+++ classification.py
@@ -177,12 +177,12 @@ def train(data_dir, num_epochs, bs, pt_dir, category_num, model_name, device, lr
 
 
 def classification():
-    os.environ['CUDA_VISIBLE_DEVICES'] = "4"
+    os.environ['CUDA_VISIBLE_DEVICES'] = "5"
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model_name = 'densenet121'
+    model_name = 'densenet161'
     data_dir = '/media/user/Disk1/caoxu/dataset/kidney/zhongshan/20240312-mass-crop-classify-5fold/'
     category_num = 2
-    bs = 168
+    bs = 128
     lr = 0.01
     num_epochs = 500
     data = 'classification-model/20240314-mass-crop-classify-'
