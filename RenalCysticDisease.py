41a3bd4cd7f4562a618ec0f553a85e16ca638f86
cao <cao_1124@163.com>
2024-04-08 13:46:49 +0800

    20240408-renal-cystic-image-crop-classify fix train path

--- RenalCysticDisease.py
+++ RenalCysticDisease.py
@@ -23,7 +23,7 @@ def classify():
     os.environ['CUDA_VISIBLE_DEVICES'] = "3"
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model_name = 'resnet50'
-    data_dir = '/media/user/Disk1/caoxu/dataset/kidney/shiyuan/20240122-renal-cystic-crop-classify-5fold/'
+    data_dir = '/media/user/Disk1/caoxu/dataset/kidney/shiyuan/20240408-renal-cystic-crop-classify-5fold/'
     category_num = 2
     bs = 128
     lr = 0.01
